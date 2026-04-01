e9ff32503624aa481af7b7ea9dc1ccef57db6fc3
Aidarkhan <doyanbaevaidarkhan@gmail.com>
2026-04-02 00:32:03 +0500

    Gin switch fixes

---
+++ handlers/author_handler.go
@@ -0,0 +1,37 @@
+﻿package handlers
+
+import (
+	"bookstore/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var users = []models.Author{
+	{ID: 1, Name: "Baltash"},
+	{ID: 2, Name: "Daulet"},
+	{ID: 3, Name: "Danchick"},
+	{ID: 4, Name: "Rahat"},
+	{ID: 5, Name: "Nursayat"},
+}
+
+func GetAuthors(c *gin.Context) {
+	c.JSON(http.StatusOK, users)
+
+}
+
+func AddAuthor(c *gin.Context) {
+	var user models.Author
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if user.Name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Name is required"})
+		return
+	}
+	user.ID = len(users) + 1
+	users = append(users, user)
+	c.JSON(http.StatusCreated, user)
+}
---
+++ handlers/book_handler.go
@@ -0,0 +1,125 @@
+﻿package handlers
+
+import (
+	"bookstore/models"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+var books = []models.Book{
+	{ID: 1, Title: "Harry Potter", AuthorID: 2, CategoryID: 1, Price: 2000},
+	{ID: 2, Title: "anime", AuthorID: 3, CategoryID: 3, Price: 3000},
+	{ID: 3, Title: "test", AuthorID: 4, CategoryID: 1, Price: 4000},
+	{ID: 4, Title: "test1", AuthorID: 2, CategoryID: 1, Price: 2000},
+	{ID: 5, Title: "test2", AuthorID: 3, CategoryID: 3, Price: 3000},
+	{ID: 6, Title: "test3", AuthorID: 4, CategoryID: 2, Price: 4000},
+	{ID: 7, Title: "test4", AuthorID: 2, CategoryID: 1, Price: 2000},
+	{ID: 8, Title: "test5", AuthorID: 3, CategoryID: 1, Price: 3000},
+	{ID: 9, Title: "test6", AuthorID: 4, CategoryID: 2, Price: 4000},
+	{ID: 10, Title: "test7", AuthorID: 2, CategoryID: 1, Price: 2000},
+	{ID: 11, Title: "test8", AuthorID: 3, CategoryID: 1, Price: 3000},
+	{ID: 12, Title: "test9", AuthorID: 4, CategoryID: 2, Price: 4000},
+}
+
+func GetBook(c *gin.Context) {
+	categoryIDStr := c.Query("category_id")
+	firstPg := c.DefaultQuery("L", "1")
+	lastPg := c.DefaultQuery("R", "2")
+
+	L, _ := strconv.Atoi(firstPg)
+	R, _ := strconv.Atoi(lastPg)
+
+	var filteredBooks []models.Book
+	for _, b := range books {
+		if categoryIDStr == "" || fmt.Sprint(b.CategoryID) == categoryIDStr {
+			filteredBooks = append(filteredBooks, b)
+		}
+	}
+
+	startIndex := (L - 1) * R
+	endIndex := startIndex + R
+
+	if startIndex >= len(filteredBooks) {
+		c.JSON(http.StatusOK, []models.Book{})
+		return
+	}
+
+	if endIndex > len(filteredBooks) {
+		endIndex = len(filteredBooks)
+	}
+
+	result := filteredBooks[startIndex:endIndex]
+	c.JSON(http.StatusOK, result)
+}
+func GetBookByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, b := range books {
+		if fmt.Sprint(b.ID) == id {
+			c.JSON(http.StatusOK, b)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "book not found"})
+}
+
+func AddBook(c *gin.Context) {
+	var book models.Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if book.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Title empty"})
+		return
+	}
+
+	if book.Price <= 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Price must be greater than 0"})
+		return
+	}
+
+	book.ID = len(books) + 1
+	books = append(books, book)
+	c.JSON(http.StatusCreated, book)
+}
+
+func UpdateBook(c *gin.Context) {
+	id := c.Param("id")
+
+	var updatedBook models.Book
+	if err := c.ShouldBindJSON(&updatedBook); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, b := range books {
+		if fmt.Sprint(b.ID) == id {
+			updatedBook.ID = b.ID
+			books[i] = updatedBook
+			c.JSON(http.StatusOK, updatedBook)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "book not found"})
+}
+
+func DeleteBook(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, b := range books {
+		if fmt.Sprint(b.ID) == id {
+			books = append(books[:i], books[i+1:]...)
+			c.JSON(http.StatusOK, gin.H{"message": "book deleted"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "book not found"})
+}
